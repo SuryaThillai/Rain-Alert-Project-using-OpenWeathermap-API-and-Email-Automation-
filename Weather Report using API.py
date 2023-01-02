15c45eb19c0543133b789a7638e59c55d4b5b906
SuryaNarayanan <87986571+SuryaThillai@users.noreply.github.com>
2023-01-02 20:25:24 +0530

    Add files via upload

---
+++ Weather Report using API.py
@@ -0,0 +1,22 @@
+import requests
+
+
+API_KEY = "7a4964cce9d6a994ca87d92bdfc214ab"
+
+ONE_CALL_API_ED = "https://api.openweathermap.org/data/2.5/onecall"
+
+ACC_SID = "ACede475a60a45b483587ff7e9a4e71a11"
+AUTH_TOK = "ed9e5394471fb6e748aea0dbd527e20b"
+
+parameters = {
+    "lat":40.7128,
+    "lon":74.0060,
+    "appid":API_KEY,
+    "exclude":"current,minutely,daily"
+}
+
+response = requests.get(ONE_CALL_API_ED,params=parameters)
+response.raise_for_status()
+data = response.json()
+
+print(data)
